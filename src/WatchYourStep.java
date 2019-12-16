d8435b78d6a0a0a8bd522e776dad6d14ae450d31
Mrs. Lombardi <lombardim@theellisschool.org>
2019-12-16 11:05:50 -0500

    randomizes hole

--- src/WatchYourStep.java
+++ src/WatchYourStep.java
@@ -1,17 +1,30 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 public class WatchYourStep extends JFrame{
 	
+	private static final int GRIDSIZE = 10;
+	//create a 2D array
+	private TerrainButton [] [] terrain = new TerrainButton [GRIDSIZE][GRIDSIZE];
+	private static final int NUMBEROFBOMBS = 10;
 
 	public WatchYourStep(){	
 		
 		initGUI();
+		setBombs();
 		
-		setTitle("Game Window");
+		setTitle("Minesweeper");
 		setSize(200, 100); //pixels
 		setResizable(true);
 		pack();
@@ -24,9 +37,47 @@ public class WatchYourStep extends JFrame{
 	}
 	
 	public void initGUI(){
+		Font titleFont = new Font("Arial", Font.BOLD, 32);
 		JLabel titleLabel = new JLabel("Watch Your Step");
-		add(titleLabel);
+		titleLabel.setFont(titleFont);
 		titleLabel.setHorizontalAlignment(JLabel.CENTER); //left or right
+		titleLabel.setForeground(Color.MAGENTA);
+		titleLabel.setBackground(Color.BLACK);
+		titleLabel.setOpaque(true);
+		add(titleLabel, BorderLayout.PAGE_START);
+		
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(GRIDSIZE, GRIDSIZE));
+		add(centerPanel, BorderLayout.CENTER);
+		//TerrainButton testButton = new TerrainButton(0, 0);
+		//centerPanel.add(testButton);
+		for (int r = 0; r < GRIDSIZE; r++) {
+			for (int c = 0; c < GRIDSIZE; c++) {
+				terrain[r][c] = new TerrainButton(r, c);
+				terrain[r][c].addActionListener(new ActionListener() {
+					public void actionPerformed (ActionEvent e) {
+						TerrainButton button = (TerrainButton) e.getSource();
+						int row = button.getRow();
+						int col = button.getCol();
+						terrain[row][col].click();
+					}
+				});
+				centerPanel.add(terrain[r][c]);
+			}
+		}
+	}
+	
+	public void setBombs() {
+		Random rand = new Random();
+		int pickRow;
+		int pickCol;
+		for (int i = 0; i < NUMBEROFBOMBS; i++) {
+			do {
+				pickRow = rand.nextInt(GRIDSIZE);
+				pickCol = rand.nextInt(GRIDSIZE);
+			}while(terrain[pickRow][pickCol].hasBomb());
+			terrain[pickRow][pickCol].setBomb();
+		}
 	}
 
 	public static void main(String[] args) {
